f0e986f564510b458b7f76b86b4aff6f751404ec
thaisday <kiddoma17@gmail.com>
2015-04-26 02:30:06 +0800

    test

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ import url
 
 settings = dict(
     template_path = os.path.join(os.path.dirname(__file__), "template"),
-    static_path = os.path.join(os.path.dirname(__file__), "css"),
+    static_path = os.path.join(os.path.dirname(__file__), "static"),
 )
 
 application = tornado.web.Application(url.url,**settings)
